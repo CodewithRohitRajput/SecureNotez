07fc9cec0a592c4f3775a89bb0c39c02a1082439
Rohit singh rajput <rohitsinghrajput2k24@gmail.com>
2025-05-13 20:18:54 +0530

    Update server.js
    added .connect

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const port =  process.env.PORT || 3000;
 
 
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI , { useNewUrlParser: true, useUnifiedTopology: true })
 .then(()=>{
     console.log("MongoDB Connected");
 })
