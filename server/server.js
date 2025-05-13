9933c558f1839cc4091a85d06c5f09f16378e530
Rohit singh rajput <rohitsinghrajput2k24@gmail.com>
2025-05-13 18:33:09 +0530

    Update server.js
    added frontend link to backend for cors

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/secureNotes')
 
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://securenotez-1.onrender.com',
   credentials: true,
 }));
 
