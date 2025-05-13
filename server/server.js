57277d801e83e9ec18595ee0cda8a798ff679b9c
Rohit singh rajput <rohitsinghrajput2k24@gmail.com>
2025-05-13 18:44:48 +0530

    Update server.js
    added port

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const noteRoutes = require('./routes/note');
 const authRoutes = require('./routes/auth');
-const port = 3000;
+const port =  process.env.PORT || 3000;
 
 
 
