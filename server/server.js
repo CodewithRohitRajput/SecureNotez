31c86b8b26fb6ef621f3f12af4cfdfa83b419b4f
Rohit singh rajput <rohitsinghrajput2k24@gmail.com>
2025-05-14 21:10:54 +0530

    test

--- server/server.js
+++ server/server.js
@@ -37,8 +37,10 @@ const corsOptions = {
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization']
 };
+
 
 app.set("trust proxy", 1);
 
