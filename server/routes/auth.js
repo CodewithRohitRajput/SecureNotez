2b7d5f310067957ab5f34081874c6adff064c82d
Manik2375 <manikkpr2375@gmail.com>
2025-05-14 22:33:35 +0530

    test

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -58,7 +58,7 @@ router.post("/login", async (req, res) => {
       sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       path: "/", // Accessible across all paths
       maxAge: 3600000,
-      domain: '.onrender.com' 
+      domain: 'onrender.com' 
     });
 
     res.status(200).json({ message: "User logged in" });
